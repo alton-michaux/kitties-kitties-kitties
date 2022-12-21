906325f6aaf4b1a174e380940170de58cca71984
Alton Michaux <alton.michaux@yahoo.com>
2022-12-21 00:19:48 -0500

    added some styling changes

--- src/cards.js
+++ src/cards.js
@@ -1,22 +1,45 @@
-import React, { useState } from "react";
-import catFact from './utils'
+import React, { useState, useEffect } from "react";
+import { catFact, getRandomCatImage } from './utils'
 
 const Card = () => {
     const [fact, setFact] = useState(null);
     const [loading, setLoading] = useState(false)
+    const [image, setImage] = useState(null)
 
     const handleCatFact = async () => {
-        setLoading(true)
+        setLoading(true);
         const catObject = await catFact();
-        setLoading(false)
+        await handleCatImage();
         setFact(catObject.fact);
+        setLoading(false);
     }
+
+    const handleCatImage = async () => {
+        setLoading(true);
+        const catPicture = await getRandomCatImage();
+        setImage(catPicture);
+        setLoading(false);
+    }
+
+    const handleInitialLoad = async () => {
+        setLoading(true);
+        const catObject = await catFact();
+        const catPicture = await getRandomCatImage();
+        setImage(catPicture);
+        setFact(catObject.fact);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        handleInitialLoad();
+    }, [])
 
     return (
         <div className="kitty-card">
             {loading ?
                 <p> Loading...</p> :
                 <>
+                    <img className="kitty-image" src={image} alt="kitties" />
                     <p>{fact}</p>
                     <button onClick={() => { handleCatFact() }}>Click Me</button>
                 </>
