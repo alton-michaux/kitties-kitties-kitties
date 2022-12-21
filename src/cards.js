180963079ea619282243d040fe27b27c4a8c708d
Alton Michaux <alton.michaux@yahoo.com>
2022-12-20 23:41:51 -0500

    added loading state variable and incorporated into design

--- src/cards.js
+++ src/cards.js
@@ -3,16 +3,24 @@ import catFact from './utils'
 
 const Card = () => {
     const [fact, setFact] = useState(null);
+    const [loading, setLoading] = useState(false)
 
     const handleCatFact = async () => {
+        setLoading(true)
         const catObject = await catFact();
+        setLoading(false)
         setFact(catObject.fact);
     }
 
     return (
         <div className="kitty-card">
-            <p>{fact}</p>
-            <button onClick={() => { handleCatFact() }}>Click Me</button>
+            {loading ?
+                <p> Loading...</p> :
+                <>
+                    <p>{fact}</p>
+                    <button onClick={() => { handleCatFact() }}>Click Me</button>
+                </>
+            }
         </div>
     )
 }
